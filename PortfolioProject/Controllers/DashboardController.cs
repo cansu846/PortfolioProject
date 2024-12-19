c92d812396a362cba9d33f2cbaa6d15eb7b97406
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-12-20 00:13:31 +0300

    feat: configure dashboard

---
+++ PortfolioProject/Controllers/DashboardController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PortfolioProject.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //public PartialViewResult ScriptPartial() {
+        //    return PartialView();
+        //}
+        public PartialViewResult HeadPartial()
+        {
+            return PartialView();
+        }
+
+        public PartialViewResult SideNavbarPartial() { 
+            return PartialView();
+        }
+
+        public PartialViewResult TopNavbarPartial() { 
+            return PartialView();
+        }
+        public PartialViewResult HeaderPartial()
+        {
+            return PartialView();   
+        }
+    }
+}
