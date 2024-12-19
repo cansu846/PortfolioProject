10fce0bbec98431c6fde17d5e95a034a55dfafd0
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-12-19 16:47:51 +0300

    feat: addde form for create skill

--- PortfolioProject/Controllers/SkillController.cs
+++ PortfolioProject/Controllers/SkillController.cs
@@ -1,4 +1,5 @@
 ﻿using BusinessLayer.Abstract;
+using EntityLayer.Concrete;
 using Microsoft.AspNetCore.Mvc;
 
 namespace PortfolioProject.Controllers
@@ -14,6 +15,19 @@ namespace PortfolioProject.Controllers
         {
             var values = _skillService.GetAll();
             return View(values);
+        }
+
+        [HttpGet]
+        public IActionResult AddSkill()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddSkill(Skill newSkill)
+        {
+            _skillService.Add(newSkill);
+            return View();
         }
     }
 }
