44f9fb72c06e803a59c8daa366bfc5bbaad31b43
Cansu Kocaoglu <cansukocaoglu55@gmail.com>
2024-12-23 17:04:45 +0300

    feat: customize user identity and role

---
+++ EntityLayer/Concrete/WriterUser.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    //Mesaj yazan kullanıcıları tanımlar
+    public class WriterUser:IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public string ImageUrl { get; set; }
+
+
+    }
+}
